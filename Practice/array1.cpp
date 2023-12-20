dfa0cb867b2358ef3f1b026f2f69d35dd629cce5
Kaap10 <vardhmangupta2004@gmail.com>
2023-12-20 23:13:56 +0530

    20/12/23

---
+++ Practice/array1.cpp
@@ -0,0 +1,87 @@
+// #include<iostream>
+// using namespace std;
+
+//Program-1 [Count 0 and 1 in an Array]
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+//Array ke elements ko bari bari dekhenge then 0 aaya toh count 0 ko badhainge ///ly with 1
+// void countZeroOne(int arr[], int size){
+//     int ZeroCount =0;
+//     int OneCount =0;
+
+//     for (int i=0; i<size; i++) {
+//         if(arr[i]==0) {
+//             ZeroCount++;
+//         }
+//         else {
+//             OneCount++;
+//         }
+//     }
+//     cout << "Zero Count is: "<<ZeroCount<<endl;
+//     cout << "One Count is: "<<OneCount<<endl;
+// }
+
+// int main() {
+//     int arr[100];
+//     int size;
+//     cout << "the size of the array is: ";
+//     cin>> size;
+
+//     for (int i=0; i<size; i++) {
+//         cin >> arr[i];
+//     }
+
+//     countZeroOne(arr,size);
+//     return 0;
+// }
+
+//Program-2 [Maximum and Minimum in an Array]
+// /*limits.h ki header file lagainge jiisse INT_MAX and INT_MIN define kar payenge/*
+// Time Complexity: O(n)
+// Space Complexity: O(1)*/
+// /* Element ko Compate krvainge INT_MAX & INT_MIN se minVal ko INT_MAX rakhenge and minMax ko INT_Min, element minVal se chota hua toh minVal hojayega similliarly wiht minMax*/
+
+// #include<iostream>
+// #include<limits.h>
+// using namespace std;
+
+// void findMinMax(int arr[], int size) {
+//     int minVal = INT_MAX;
+//     int maxVal = INT_MIN;
+
+//     for (int i=1; i<size; i++) {
+//         if(arr[i]<minVal) {
+//             minVal = arr[i];
+//         }   
+//         else {
+//             maxVal = arr[i];
+//         }
+//      }
+//      cout << "Minimum Value is: "<<minVal<<endl;
+//      cout << "Maximum Value is: "<<maxVal<<endl;
+// }
+
+
+// int main() {
+//     int arr[100];
+//     int size;
+
+//     cout << "Enter the size: ";
+//     cin >> size;
+
+//     for (int i=0; i<size; i++) {
+//         cin >> arr[i];
+//     }
+
+//     findMinMax(arr, size);
+//     return 0;
+// }
+
+// #Program-3 [Reverse an Array]
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+#include<iostream>
+using namespace std;
+
+
