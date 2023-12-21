cace8a0e0f046c908b65d036e53afdc9f13216cc
Kaap10 <vardhmangupta2004@gmail.com>
2023-12-21 22:14:30 +0530

    updated 21/12/23

---
+++ LeetCode/MissingNumber_268.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        //missing number find krna hai 
+
+        int n= nums.size();
+        int sum =0;
+
+        for (int i=0; i<n; i++) {
+            sum = sum + nums[i]; //given no ka sum kal lenge
+        }
+
+        int totalSum = (n*(n+1))/2; //sum of range nikal lenge bu using n*(n+1)/2 formual se
+        int missingNum = totalSum - sum; //sum fo ranges - sum of given range 
+
+        return missingNum;
+    }
+};
