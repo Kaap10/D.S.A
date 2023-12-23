74228495c9039b1cf9e01ed1932e5a696eafdff0
Kaap10 <vardhmangupta2004@gmail.com>
2023-12-23 11:32:40 +0530

    23/12/23

---
+++ Practice/Arrays/CharArray1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    char ch[10];
+    for(int i=0; i<=10; i++) {
+        cin >> ch[i];
+    }
+
+    for (int i=0; i<=10; i++) {
+        cout << ch[i] <<" ";
+    }
+}
