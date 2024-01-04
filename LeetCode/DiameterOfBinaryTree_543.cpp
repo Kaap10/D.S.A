ea352788321b9ceff85d4f7efe917b635dab7346
Kaap10 <vardhmangupta2004@gmail.com>
2024-01-04 14:50:28 +0530

    Updated 4/1/23

---
+++ LeetCode/DiameterOfBinaryTree_543.cpp
@@ -0,0 +1,65 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// class Solution {
+// public:
+//     //OPTIMAL SOLUTIONS HAI (No overhead recursive calls, diameter baar baar nhi nikalna pd rha hai)
+//     int heigth(TreeNode* root, int &diameter) {
+//         if (root==NULL) {
+//             return 0;
+//         }
+
+//         int LH = heigth(root->left, diameter);
+//         int RH = heigth(root->right, diameter);
+//         int maxHeigth= max(LH, RH) +1;
+//         diameter= max(diameter, (LH+RH));
+//         return maxHeigth;
+//     }
+//     int diameterOfBinaryTree(TreeNode* root) {
+
+//         //base case
+//         if(root==NULL) {
+//             return 0;
+//         }      
+
+//         int diameter =0;
+//         heigth(root, diameter);
+//         return diameter;
+//     }
+// };
+
+// BRUTE FORCE Solutions(Overhead recursive calls hai, baar baar recursive class nikalni pd rha hai)
+class Solution {
+public:
+
+      int height(TreeNode* root) {
+          if (root==NULL) {
+              return 0;
+          }
+
+          int LH = height(root->left);
+          int RH = height(root->right);
+          int finalHeight=max(LH, RH) +1;
+          return finalHeight;
+      }
+      int diameterOfBinaryTree(TreeNode* root) {
+          //base case
+          if(root==NULL) {
+              return 0;
+          }
+
+          int option1 = diameterOfBinaryTree(root->left);
+          int option2 = diameterOfBinaryTree(root->right);
+          int option3 = height(root->left) + height(root->right);
+          int diameter = max(option1, max(option2, option3));
+          return diameter;
+      }
+};
--- Practice/Arrays/BinaryTree1.cpp
+++ Practice/Arrays/BinaryTree1.cpp
@@ -1,42 +1,86 @@
+// #include<iostream>
+// using namespace std;
+
+// //ab class baninge
+// class Node{
+//     public:
+//            int data;
+//            Node* left;
+//            Node* right;
+
+//            Node(int val) {
+//             this->data=val;
+//             this->left=NULL;
+//             this->right=NULL;
+//            }
+// };
+
+// // New node banainge, jo ki Root Node dega
+
+// Node* createTree() {
+//     cout<<"Enter the value: " <<endl;
+//     int data;
+//     cin>>data;
+
+//     //base case
+//     if(data==-1) {
+//         return NULL;
+//     }
+//     //new data fill krenge
+//     Node* root = new Node(data);
+//     //create left subtreee
+//     root -> left = createTree();
+//     //create right subtree
+//     root -> right = createTree();
+
+//     return root;
+// }
+
+// int main() {
+//     Node* root = createTree();
+//     return 0;
+// }
+
+//Generic Tree Problem
 #include<iostream>
+#include<queue>
+
 using namespace std;
 
-//ab class baninge
-class Node{
+//pehle Node banainge
+class Node {
     public:
-           int data;
-           Node* left;
-           Node* right;
+    int data;
+    int children_count;
+    Node** children;
 
-           Node(int val) {
-            this->data=val;
-            this->left=NULL;
-            this->right=NULL;
-           }
+    //CONSTRUCTOR baninnge
+    Node(int value) {
+        this->data= value;
+        this->children_count=0;
+        this -> children = NULL;
+
+    }
 };
 
-// New node banainge, jo ki Root Node dega
+Node* takeInput() {
+    int data, count;
 
-Node* createTree() {
-    cout<<"Enter the value: " <<endl;
-    int data;
+    cout <<"Enter Root data:" ;
     cin>>data;
 
-    //base case
-    if(data==-1) {
-        return NULL;
+    cout << "Enter children count for "<<data<<" node: ";
+    cin>>count;
+
+    Node* root = new Node(data); //create Node
+    root-> children_count = count; //creating children count
+
+    //ek dynamic array banainge jisse chiildre node link hoga
+    root->children= new Node(count);
+    for(int i=0; i<count; i++) {
+
     }
-    //new data fill krenge
-    Node* root = new Node(data);
-    //create left subtreee
-    root -> left = createTree();
-    //create right subtree
-    root -> right = createTree();
 
-    return root;
-}
 
-int main() {
-    Node* root = createTree();
-    return 0;
+
 }
