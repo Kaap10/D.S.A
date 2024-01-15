1928afec3c06bcbe69543b22baf0a8eda8ba684b
Kaap10 <vardhmangupta2004@gmail.com>
2024-01-15 17:30:23 +0530

    update 15/1/24

--- Practice/Restart/15.cpp
+++ Practice/Restart/15.cpp
@@ -124,30 +124,188 @@ using namespace std;
 // }
 
 //mereko prime num print krna hai between two numbers
-int main() {
-    int low, high, i;
-    bool is_prime = true;
+// int main() {
+//     int low, high, i;
+//     bool is_prime = true;
 
-    cout << "Enter the two numbers: ";
-    cin >> low >> high;
+//     cout << "Enter the two numbers: ";
+//     cin >> low >> high;
 
-    while(low<=high) {
-        is_prime = true;
+//     while(low<=high) {
+//         is_prime = true;
 
-        if(low==0 || low==1) {
-            is_prime = false;
-        }
+//         if(low==0 || low==1) {
+//             is_prime = false;
+//         }
 
-        for(int i=0; i<=low/2; i++) {
-            if(low%i==0) {
-                is_prime = false;   
-                break;
-            }
-        }
+//         for(int i=0; i<=low/2; i++) {
+//             if(low%i==0) {
+//                 is_prime = false;   
+//                 break;
+//             }
+//         }
 
-        if(is_prime) 
-        cout << low << " ";
-        low ++;
-    }
-    return 0;
-}
+//         if(is_prime) 
+//         cout << low << " ";
+//         low ++;
+//     }
+//     return 0;
+// }
+
+//mereko armstrong nikalna hai ok?
+// 153 = 1 cube + 5 cube + 3 cube krke = 153 ke qual aana chaiye 
+
+// int main() {
+//     int n, orgNum, rem, result;
+//     result = 0;
+
+//     cout << "Enter the number: ";
+//     cin >> n;
+
+//     orgNum = n;
+
+//     while(orgNum!=0) {
+//         rem = orgNum%10;
+//         result = result + (rem*rem*rem);
+//         orgNum = orgNum/10;
+//     }
+//     if(result == n) {
+//         cout << "Num is Armstrong";
+//     }
+//     else {
+//         cout << "Num is not armstront";
+//     }
+
+//     return 0;
+// }
+
+//factors chaiye mereko kisi number ka
+// int main() {
+//     int n;
+//     cout << "Enter the number: ";
+//     cin >> n;
+
+//     for (int i=1; i<=n; i++) {
+//         if(n%i==0) {
+//             cout << i << ", ";
+//         }
+//     }
+//     return 0;
+// }
+
+//calculator using switch case
+// int main() {
+//     int num1, num2;
+//     char Op;
+
+//     cout << "Enter Num1: ";
+//     cin >> num1;
+
+//     cout << "Enter Num2: ";
+//     cin >> num2;
+
+//     cout << "Enter the operation to be performed: ";
+//     cin >> Op;
+
+//     switch(Op) {
+//         case '+':
+//         cout << num1 << " + "<<num2<<" = "<<num1+num2;
+//         break;
+
+//         case '-':
+//         cout << num1 << " - "<<num2<<" = "<<num1-num2;
+//         break;
+
+//         case '*':
+//         cout << num1 << " * "<<num2<<" = "<<num1*num2;
+//         break;
+
+//         case '/':
+//         cout << num1 << " / "<<num2<<" = "<<num1/num2;
+//         break;
+
+//         default:
+//         cout << "Error Operator!";
+//         break;
+//     }
+
+//     return 0;
+// }
+
+//Prime number between two intervals using function
+
+// int check_prime(int n) {
+//     bool is_prime = true;
+
+//     if(n==0 || n==1) {
+//         is_prime = false;
+//     }
+//     for(int j=2; j<=n/2; j++ ) {
+//         if(n%j==0) {
+//             is_prime = false;
+//             break;  
+//         }
+//     }
+//     return is_prime;
+// }
+// int main() {
+//     int n1, n2;
+//     cout << "Enter n1 and n2: ";
+//     cin >> n1 >> n2;
+
+//     if(n1>n2) {
+//         n2 = n1 + n2;
+//         n1 = n2 - n1;
+//         n2 = n2 - n1;
+//     }
+
+//     for(int i=n1+1; i<n2; i++) {
+//         bool flag = check_prime(i);
+
+//         if(flag) {
+//             cout << i << " ,";
+//         }
+//     }
+//     return 0;
+// }
+
+// bool check_prime(int n) {
+//     int i;
+//     bool is_prime = true;
+
+//     if(n==0 || n==1) {
+//         is_prime  = false;
+//     }
+
+//     for(i=2; i<=n/2; i++) {
+//         if(n%i==0) {
+//             is_prime = false;
+//             break;
+//         }
+//     }
+//     return is_prime;
+// }
+
+// int main() {
+//     int n, i;
+//     bool flag = false;
+
+//     cout << "Enter a positive integer: ";
+//     cin >> n;
+
+//     for(int i=2; i<= n/2; i++) {
+//         if(check_prime(i)) {
+//             if(check_prime(n-i)) {
+//                 cout << n << " + "<<i<<" = "<<  n-1<<endl;
+//                 flag = true;
+//             }
+//         }
+//     }
+
+//     if(!flag) {
+//         cout << n;
+//     }
+
+//     return 0;
+// }
+
