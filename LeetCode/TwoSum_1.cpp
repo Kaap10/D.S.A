b7b9ce591f9c0c95b1cafb3ef3e9b50c2b97f1c5
Kaap10 <vardhmangupta2004@gmail.com>
2024-01-19 19:51:04 +0530

    updated 19/1/24

--- LeetCode/TwoSum_1.cpp
+++ LeetCode/TwoSum_1.cpp
@@ -1,14 +1,33 @@
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         int n = nums.size();
+//         for (int i = 0; i < n - 1; i++) {
+//             for (int j = i + 1; j < n; j++) {
+//                 if (nums[i] + nums[j] == target) {
+//                     return {i, j};
+//                 }
+//             }
+//         }
+//         return {}; 
+//     }
+// };
+
+//[USING HASHING] ->
+
 class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    public:
+    vector<int> twoSum(vector<int> &nums, int target) {
+        map<int, int>mpp;
         int n = nums.size();
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return {i, j};
-                }
+        for(int i=0; i<n; i++) {
+            int num = nums[i];
+            int neededNum = target - num;
+            if(mpp.find(neededNum) != mpp.end()) {
+                return {mpp[neededNum], i};
             }
+            mpp[num] = i;
         }
-        return {}; 
+        return {-1,-1};
     }
 };
