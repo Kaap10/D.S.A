fa5a093e73a631b3780b6c5e0aeb33c867b190af
Kaap10 <vardhmangupta2004@gmail.com>
2024-01-24 01:56:33 +0530

    updated 24/1/24

--- Practice/Linked List/Restart/ll3.cpp
+++ Practice/Linked List/Restart/ll3.cpp
@@ -33,28 +33,180 @@ using namespace std;
 
 
 //Now Recursion Approach ->
-class Solution{
-    public:
+// class Solution{
+//     public:
 
-    Node* reverseListUsingRecursion(Node* prev, Node* curr) {
-        //base case
-        if(curr = NULL) {
-            return prev;
-        }
+//     Node* reverseListUsingRecursion(Node* prev, Node* curr) {
+//         //base case
+//         if(curr = NULL) {
+//             return prev;
+//         }
 
-        Node* nextNode = curr -> next;
-        curr->next = prev;
-        prev = curr;
-        curr = nextNode;
+//         Node* nextNode = curr -> next;
+//         curr->next = prev;
+//         prev = curr;
+//         curr = nextNode;
 
-        Node* recursionKaAns = reverseListUsingRecursion(prev, curr);
-        return recursionKaAns;
-    }
+//         Node* recursionKaAns = reverseListUsingRecursion(prev, curr);
+//         return recursionKaAns;
+//     }
 
-    Node* reverseList(Node* head) {
-        Node* prev = NULL;
-        Node* curr = head;
+//     Node* reverseList(Node* head) {
+//         Node* prev = NULL;
+//         Node* curr = head;
 
-        return reverseListUsingRecursion(prev, curr);
-    }
-}
+//         return reverseListUsingRecursion(prev, curr);
+//     }
+// }
+
+
+//Reverse Linked List LeetCode - 234
+//-> middle of LL, reverse of LL using recursion, compareList
+// class Solution {
+//     public:
+
+//     Node* middleLL(Node* head) {
+//         //using hare and tortoise algorithm
+//         Node* slow = head;
+//         Node* fast = head;
+
+//         while(fast->next!=NULL) {
+//             fast = fast -> next;
+//             if(fast->next!=NULL) {
+//                 fast = fast -> next;
+//                 slow = slow ->next;
+//             }
+//         }
+//         return slow;
+//     }
+
+//     Node* reverseLL(Node* prev, Node* curr) {
+//         if(curr = NULL) {
+//             return prev;
+//         }
+
+//         while(curr!=NULL) {
+//             Node* nextNode = curr -> next;
+//             curr -> next = prev;
+//             prev = curr;
+//             curr = nextNode;
+
+//             return reverseLL(prev, curr);
+//         }
+//     }
+
+//     bool compareList(Node* head, Node* head2) {
+//         while(head2!=NULL) {
+//             if(head->val != head2->val) {
+//                 return false;
+//             }
+
+//             else {
+//                 head = head -> next;
+//                 head2 = head2 -> next;
+//             }
+//         }
+//         return true;
+//     }
+
+//     Node* isPalindrome(Node* head) {
+//         Node* midNode = middleLL(head);
+//         Node* head2 = midNode -> next;  
+//         midNode -> next = NULL;
+
+//         Node* prev = NULL;
+//         Node* curr = head2;
+//         head2 = reverseListUsingRecursion(prev, curr);
+
+//         bool ans = compareList(head, head2);
+//         return ans;
+//     }
+// };
+
+//Linked List Cycle
+// class Solution {
+//     public:
+
+//     bool hasCycle(ListNode* head) {
+//         map<Node*, bool> table;
+
+//         Node* temp = head;
+
+//         while(temp!=NULL) {
+//             if(table[temp] == false) {
+//                 table[temp] = true;
+//             }
+
+//             else {
+//                 return true;
+//             }
+
+//             temp = temp->next;
+//         }
+//     }
+//     return true;
+// }
+
+// class Solution{
+//     public:
+    
+//     //hare and tortoise algorithm
+//     Node* slow = head;
+//     Node* fast = head;
+
+//     while(fast!=NULL) {
+//         fast = fast -> next;
+//         if(fast!=NULL) {
+//             fast = fast -> next;
+//             slow = slow -> next;
+//         }
+
+//         if(slow == fast) {
+//             return true;
+//         }
+//     }
+
+//     return false;
+// }
+
+//Starting point of loop (leetcode - 142)
+// -> 1. loop present hai ya nhi check krenge
+// -> 2. slow ko head pe rakhenge? fhir main cheez slow and fast 1 1 step chlege, jab slow=fast hoga tab break hojayega, and return krenge slow
+
+// class Solution{
+//     public:
+
+//     Node* detectcycle(Node* head) {
+//         Node* slow = head;
+//         Node* fast = head;
+
+//         while(fast!=NULL) {
+//             fast = fast-> next;
+//             if(fast!=NULL) {
+//                 fast = fast->next;
+//                 slow = slow -> next;
+//             }
+
+//             if(slow == fast ) {
+//                 break;
+//             }
+//         }
+
+//         if(fast == NULL) {
+//             return false;
+//         }
+
+//         slow = head;
+
+//         while(fast != slow) {
+//             slow = slow -> next;
+//             fast = fast ->next;
+//         }
+//         return slow;
+//     }
+// };
+
+
+
+
+
