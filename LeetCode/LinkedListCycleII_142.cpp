05d39b65f021829086f070111d04022bc461d6d9
Kaap10 <vardhmangupta2004@gmail.com>
2024-01-24 15:00:34 +0530

    update 24/1/24

--- LeetCode/LinkedListCycleII_142.cpp
+++ LeetCode/LinkedListCycleII_142.cpp
@@ -14,27 +14,22 @@ public:
 
         while(fast!=NULL) {
             fast = fast -> next;
-
             if(fast!=NULL) {
                 fast = fast -> next;
                 slow = slow -> next;
             }
-
             if(slow == fast) {
                 break;
             }
         }
-
         if(fast==NULL) {
             return fast;
         }
-
         slow = head;
         while(fast!=slow) {
             slow = slow -> next;
             fast = fast -> next;
         }
-
         return slow;
         
     }
--- Practice/Linked List/ll4.cpp
+++ Practice/Linked List/ll4.cpp
@@ -48,50 +48,139 @@ using namespace std;
 
 //Add 1 to a linked list
 
-void reverseLL(Node* &head) {
-    Node* prev = NULL;
-    Node* curr = head;
+// void reverseLL(Node* &head) {
+//     Node* prev = NULL;
+//     Node* curr = head;
 
-    while(curr!=NULL) {
-        Node* nextNode = curr -> next;
-        curr ->next = prev;
-        prev = curr;
-        curr = nextNode;
-    }
-    return prev;
-}
-void addOneToLL(Node* &head) {
-    head = reverseLL(head);
+//     while(curr!=NULL) {
+//         Node* nextNode = curr -> next;
+//         curr ->next = prev;
+//         prev = curr;
+//         curr = nextNode;
+//     }
+//     return prev;
+// }
+// void addOneToLL(Node* &head) {
+//     head = reverseLL(head);
 
-    //add one to linked list
-    Node* temp = head;
-    int carry = 1;
+//     //add one to linked list
+//     Node* temp = head;
+//     int carry = 1;
 
-    while(temp->next != NULL) {
-        int TotalSum = temp -> data + carry;
-        carry = TotalSum/10;
-        int digit = TotalSum % 10;
+//     while(temp->next != NULL) {
+//         int TotalSum = temp -> data + carry;
+//         carry = TotalSum/10;
+//         int digit = TotalSum % 10;
 
-        temp -> data = digit;
-        temp = temp->next;
+//         temp -> data = digit;
+//         temp = temp->next;
 
-        if(carry==0) {
-            break;
+//         if(carry==0) {
+//             break;
+//         }
+//     }
+
+//     //-> jab carry zero na ho and list khtm na huyi usko alag se handle krna pdega 
+//     if(carry!=0) {
+//         int totalSum = temp->data + carry;
+//         carry = totalSum/10;
+//         int digit = totalSum % 10;
+
+//         if(carry!=0) {
+//             Node* newNode = new Node(carry);
+//             temp -> next = newNode;
+//         }
+//     }
+
+//     //again reverse krdo
+//     head = reverseLL(head);
+// }
+
+//Reverse Nodes in K-Group
+// -> Mast Question hai 
+
+// class Solution {
+//     public: 
+//     /*
+//     1. getLength ka function banao
+//     2. main function ke ander->
+//     (a) 1 case mai solve krunga
+//     (b) ab recursion solve krega
+//     (c) last mai kuch IMP Cheezen hai to keep in MIND*/
+
+//     int getLength(Node* head) {
+//         Node* temp = head;
+//         int count = 0;
+//         while(temp!=NULL) {
+//             count++;
+//             temp = temp -> next;
+//         }
+//         return count;
+//     }
+
+//     Node* reverseKgroup(Node* head, int k) {
+
+//         //base case
+//         int len = getLength(head);
+//         if(len < k) {
+//             return head;
+//         }
+
+//         //1 case mai solve krunga ab
+//         Node* prev = NULL;
+//         Node* curr = head;
+//         Node* currNode = curr -> next;
+//         int position = 0;
+        
+//         while(position<k) {
+//             nextNode = curr->next;
+//             curr->next = prev;
+//             prev = curr;
+//             curr = nextNode;
+//             position++;
+//         }
+
+//         Node* recursionKaAns = NULL;
+//         if(nextNode!=NULL) {
+//             recursionKaAns = reverseKgroup(nextNode, k);
+
+//             head->next = recursionKaAns;
+//         }
+
+//         return prev;
+//     }
+
+// }
+
+//Remove Duplicates from sorted Linked List
+
+class Solution{
+    public:
+    Node* deleteDuplicates(Node* head) {
+        //corner cases -> empty list & single element
+        if(head == NULL) {
+            return head;
         }
-    }
 
-    //-> jab carry zero na ho and list khtm na huyi usko alag se handle krna pdega 
-    if(carry!=0) {
-        int totalSum = temp->data + carry;
-        carry = totalSum/10;
-        int digit = totalSum % 10;
-
-        if(carry!=0) {
-            Node* newNode = new Node(carry);
-            temp -> next = newNode;
+        if(head-> next = NULL) {
+            return head;
         }
-    }
 
-    //again reverse krdo
-    head = reverseLL(head);
+        Node* temp = head;
+
+        while(temp!=NULL) {
+            if(temp-> next != NULL && temp->val == temp -> next -> val) {
+                Node* nextNode = temp ->next;
+                temp->next = nextNode -> next;
+                nextNode -> next = NULL;
+                delete nextNode;
+            }
+
+            else {
+                temp = temp -> next;
+            }
+        }
+
+        return head;
+    }
 }
