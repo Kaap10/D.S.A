c4f8c693a57280e14434de0945e08cc2ead3b82c
Kaap10 <vardhmangupta2004@gmail.com>
2024-01-26 00:47:02 +0530

    update 25/1/24

--- Practice/CharArraysString/CAS2.cpp
+++ Practice/CharArraysString/CAS2.cpp
@@ -63,4 +63,68 @@ using namespace std;
 
 //     return 0;
 // }
+ 
+//program 3 (upper case to lower case & lowercase to uppercase)
 
+//Lowercase to Uppercase
+
+// void replaceCharacter(char ch[], int size) {
+//     int index= 0;
+
+//     while(ch[index]!='\0') {
+//         if(ch[index]=='@') {
+//             ch[index] = ' ';
+//         }
+//         index++;
+//     }
+// }
+// void convertToUppercase(char ch[], int size) {
+//     int index = 0;
+//     while(ch[index]!='\0') {
+//         if(ch[index]>='a' && ch[index]<='z') {
+//             ch[index] = ch[index] - 'a' + 'A';
+//         }
+//         index++;
+//     }
+// }
+
+// void convertToLowercase(char ch[], int size) {
+//     int index = 0;
+//     while(ch[index]!='\0') {
+//         if(ch[index]>='A' && ch[index]<='Z') {
+//             ch[index] = ch[index] - 'A' +'a';
+//         }
+//         index++;
+//     }
+// }
+
+// int main() {
+//     char ch[100];
+//     cin.getline(ch,100);
+//     int length = strlen(ch);
+//     // convertToLowercase(ch, length);
+//     // cout << ch << endl;
+//     // convertToUppercase(ch, length);
+//     // cout << ch << endl;
+//     replaceCharacter(ch, length);
+//     cout << ch;
+
+//     return 0;
+// }
+
+//PROGRAM-05
+//Check Palindrom
+
+bool checkPalinrome(char ch[], int size) {
+    int start = 0;
+    int end = size-1;
+
+    while(start<=end) {
+        if(ch[start]!=ch[end]) {
+            return false;
+        }
+        start++;
+        end--;
+    }
+    return true;
+}
