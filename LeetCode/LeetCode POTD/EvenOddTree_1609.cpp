f24ca00a0176c3ed9355dc23ea33bb9fb317c9c0
Vardhman Gupta <vardhmangupta2004@gmail.com>
2024-02-29 18:50:52 +0530

    update 29/02/24

---
+++ LeetCode/LeetCode POTD/EvenOddTree_1609.cpp
@@ -0,0 +1,82 @@
+// class Solution {
+// public:
+//     bool isEvenOddTree(TreeNode* root) {
+//         queue<TreeNode*> que;
+        
+//         que.push(root);
+//         bool even_level = true;
+        
+//         while(!que.empty()) {
+            
+//             int n = que.size();
+//             int prev;
+//             if(even_level)
+//                 prev = INT_MIN;
+//             else
+//                 prev = INT_MAX;
+            
+//             while(n--) {
+                
+//                 TreeNode* node = que.front();
+//                 que.pop();
+                
+//                 if (even_level && (node->val % 2 == 0 || node->val <= prev)) {
+//                     return false;
+//                 }
+//                 if (!even_level && (node->val % 2 == 1 || node->val >= prev)) {
+//                     return false;
+//                 }
+                
+ 
+                
+//                 prev = node->val;
+//                 if(node->left) {
+//                     que.push(node->left);
+//                 }
+//                 if(node->right) {
+//                     que.push(node->right);
+//                 }
+                
+//             }
+            
+//             even_level = !even_level;
+            
+//         }
+        
+//         return true;
+//     }
+// };
+
+class Solution {
+public:
+    
+    vector<int> levelPrev;
+    
+    bool solve(TreeNode* root, int level) {
+        if(!root) {
+            return true;
+        }
+        
+        if((level%2 == 0 && root->val%2 == 0) || (level%2 != 0 && root->val%2 != 0)) {
+            return false;
+        }
+        
+        if(level >= levelPrev.size()) {
+            levelPrev.resize(level+1);
+        }
+
+        if(levelPrev[level] != 0) { //already there is a value at this level
+            if((level%2 == 0 && root->val <= levelPrev[level]) ||
+               (level%2 != 0 && root->val >= levelPrev[level]))
+                return false;
+        }
+        
+        levelPrev[level] = root->val;
+        
+        return solve(root->left, level+1) && solve(root->right, level+1);
+    }
+    
+    bool isEvenOddTree(TreeNode* root) {
+        return solve(root, 0);
+    }
+};
