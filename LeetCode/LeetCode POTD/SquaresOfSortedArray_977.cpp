0831a2f654b91eeb5b317c3ae5fefa29bab89100
Vardhman Gupta <vardhmangupta2004@gmail.com>
2024-03-02 13:50:10 +0530

    updated 2/03/24

--- LeetCode/LeetCode POTD/SquaresOfSortedArray_977.cpp
+++ LeetCode/LeetCode POTD/SquaresOfSortedArray_977.cpp
@@ -1,15 +1,15 @@
-// //Brute force
-// class Solution {
-// public:
-//     vector<int> sortedSquares(vector<int>& nums) {
-//         int n = nums.size();
-//         for(int i=0; i<n; i++) {
-//             nums[i]= nums[i]*nums[i];
-//         }
-//         sort(nums.begin(), nums.end());
-//         return nums;
-//     }
-// };
+//Brute force
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n = nums.size();
+        for(int i=0; i<n; i++) {
+            nums[i]= nums[i]*nums[i];
+        }
+        sort(nums.begin(), nums.end());
+        return nums;
+    }
+};
 
 //optimal approach (using two pointers)
 class Solution {
