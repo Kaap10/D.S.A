b3330d2575334d03e2478db4ce22891917096fe6
Vardhman Gupta <vardhmangupta2004@gmail.com>
2024-03-11 16:24:56 +0530

    update 11/03/24

--- LeetCode/LeetCode POTD/CustomSortString_791.cpp
+++ LeetCode/LeetCode POTD/CustomSortString_791.cpp
@@ -13,3 +13,50 @@ public:
     }
 };
 string Solution::orderStr;
+
+//Approach-1
+class Solution {
+public:
+    string customSortString(string order, string str) {
+        int count[26] = {0};
+        
+        for(char &x : str)
+            count[x-'a']++;
+        
+        string result = "";
+        for(char &ch : order) {
+            while(count[ch-'a']--) {
+                result.push_back(ch);
+            }
+        }
+        
+        //Add the characters which are not present in 'order'
+        for(char &ch : str) {
+            if(count[ch-'a'] > 0) {
+                result.push_back(ch);
+            }
+        }
+        
+        return result;
+    }
+};
+
+// Approach-3 (using Lambda comparator)
+class Solution {
+public:
+    string customSortString(string order, string str) {
+        vector<int> index(26, -1);
+        
+        for(int i = 0; i<order.length(); i++)
+            index[order[i]-'a'] = i;
+        
+        
+        auto lambda = [&index](char &ch1, char &ch2) {
+            return index[ch1-'a'] < index[ch2-'a'];
+        };
+        
+        sort(begin(str), end(str), lambda);
+        return str;
+        
+    }
+};
