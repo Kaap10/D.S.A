0b4f6a0617bac79190a3e295a83f144fd01d7370
Vardhman Gupta <vardhmangupta2004@gmail.com>
2024-04-01 11:15:09 +0530

    update 01/04/24

---
+++ LeetCode/LeetCode POTD/LengthofLastWord_58.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        // int  = s.length();
+        int length = 0;
+        bool counting= false;
+
+        for(int i= s.length()-1; i>=0; i--) {
+            if(s[i]!=' ') {
+                counting = true;
+                length++;
+            }
+
+            else if(counting) {
+                break;
+            }
+        }
+        return length;
+    }
+};
