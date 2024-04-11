6a7ebe7e4b54c8aa6a5727b7dcf9bc95f611cd70
Vardhman Gupta <vardhmangupta2004@gmail.com>
2024-04-11 19:43:39 +0530

    update 11/04/24

---
+++ LeetCode/LeetCode POTD/RemoveKDigits_402.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        
+        string ans = ""; 
+        int n = num.length();
+        
+        for(int i = 0; i < n; i++) {
+            
+            while(ans.length() > 0 && ans.back() > num[i] && k > 0) {
+                ans.pop_back();
+                k--;
+            }
+            
+            if(ans.length() > 0 || num[i] != '0') {
+                ans.push_back(num[i]); 
+            }
+            
+        }
+        
+        
+        while(ans.length() > 0 && k > 0) {
+            ans.pop_back();
+            k--;
+        }
+
+        if(ans == "") {
+            return "0";
+        }
+        
+        return ans;
+        
+    }
+};
