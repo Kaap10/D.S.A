639301c7cfe0f97fdbbf0096a67efb8cbbf4a960
Vardhman Gupta <vardhmangupta2004@gmail.com>
2024-04-12 22:04:45 +0530

    updated 12/04/24

---
+++ Striver A2Z Sheet/STEP-2 [Sorting Techniques]/Lec-1/Bubble_Sort.cpp
@@ -0,0 +1,13 @@
+void bubbleSort(vector<int>& arr, int n) 
+{
+    // int n = arr.size();
+    for (int i = n - 1; i >= 0; i--) {
+        for (int j = 0; j <= i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}   
---
+++ Striver A2Z Sheet/STEP-2 [Sorting Techniques]/Lec-1/Insert_Sort.cpp
@@ -0,0 +1,12 @@
+void insertionSort(int arr[], int n)
+{
+    for (int i = 0; i <= n - 1; i++) {
+        int j = i;
+        while (j > 0 && arr[j - 1] > arr[j]) {
+            int temp = arr[j - 1];
+            arr[j - 1] = arr[j];
+            arr[j] = temp;
+            j--;
+        }
+    }
+}
