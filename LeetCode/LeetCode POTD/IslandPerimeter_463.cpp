d8e6c36111d956123268b81e7bdd12c8b32dcc46
Vardhman Gupta <vardhmangupta2004@gmail.com>
2024-04-18 19:26:01 +0530

    update 18/04/24

--- LeetCode/LeetCode POTD/IslandPerimeter_463.cpp
+++ LeetCode/LeetCode POTD/IslandPerimeter_463.cpp
@@ -43,3 +43,35 @@ public:
         return -1;
     }
 };
+
+//APPROACH-2
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        int perimeter = 0;
+        for(int i = 0; i<m; i++) {
+            for(int j = 0; j<n; j++) {
+                if(grid[i][j] == 0)
+                    continue;
+                
+                if(i-1 < 0 || grid[i-1][j] == 0) //up
+                    perimeter++;
+                
+                if(i+1 >= m || grid[i+1][j] == 0) //down
+                    perimeter++;
+                
+                if(j-1 < 0 || grid[i][j-1] == 0) //left
+                    perimeter++;
+                
+                if(j+1 >= n || grid[i][j+1] == 0) //right
+                    perimeter++;
+                
+            }
+        }
+        
+        return perimeter;
+    }
+};
