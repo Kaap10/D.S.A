7b7c5809c6da4a1884bd9ebeb5c146ea8f7a33a9
Vardhman Gupta <vardhmangupta2004@gmail.com>
2024-05-22 19:27:20 +0530

    update 22/05/25

---
+++ Practice/90 Days/bst1.cpp
@@ -0,0 +1,16 @@
+//Binary Search
+
+int main() {
+    int arr[100];
+    int n = arr.size();
+    int start = 0;
+    int end = n-1;
+
+    while(start<=end) {
+        int mid = start + (end-start)/2;
+
+        if(mid > arr[i]) {
+            end = 
+        }
+    }
+}
