72e979032c52b7db610ba75c8073a638a29d222a
Vardhman Gupta <vardhmangupta2004@gmail.com>
2024-06-03 18:18:22 +0530

    update 03/06/24

--- Practice/90 Days/bst1.cpp
+++ Practice/90 Days/bst1.cpp
@@ -1,26 +1,45 @@
-Node* insertIntoBST(Node* root, int data) {
-    if(root==NULL) {
-        Node* root = new Node(data);
-        return root;
-    }
+// Node* insertIntoBST(Node* root, int data) {
+//     if(root==NULL) {
+//         Node* root = new Node(data);
+//         return root;
+//     }
 
-    if(data < root->val) {
-        root->left = insertIntoBST(root->left, data);
-    }
+//     if(data < root->val) {
+//         root->left = insertIntoBST(root->left, data);
+//     }
 
-    else {
-        root->right = insertIntoBST(root->right, data);
-    }
-    return root;
+//     else {
+//         root->right = insertIntoBST(root->right, data);
+//     }
+//     return root;
+// }
+
+// void createBST(Node* &root) {
+//     cout << "Enter the root value: ";
+//     int data;
+//     cin >> data;
+
+//     while(data!=-1) {
+//         root = insertIntoBST(root, data);
+//         cin >> data;
+//     }
+// }
+
+#include<iostream>
+using namespace std;
+
+int natural_num(int n, int x) {
+
+    if(n==0) return 0;
+    if(n==1) return 1;
+	for(int i=1; i<=n; i++) {
+		if(i!=x) {
+			cout << i << " ";
+		}
+	}
 }
-
-void createBST(Node* &root) {
-    cout << "Enter the root value: ";
-    int data;
-    cin >> data;
-
-    while(data!=-1) {
-        root = insertIntoBST(root, data);
-        cin >> data;
-    }
+int main() {
+	int n, x;
+	cin >> n >> x;
+	natural_num(n,x);
 }
